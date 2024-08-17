245a5de5a20c8ddefb5cfb87d1d3fd25e83ab34d
ParamiJayathilaka <thinithiparami53@gmail.com>
2024-08-17 14:59:34 +0530

    done

--- src/components/reservation/Reservation.jsx
+++ src/components/reservation/Reservation.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 const Reservation = () => {
   return (
-    <div>
-      
+    <section className="reservation container" id='reservation'>
+                <h2 className="section__title" data-title='Reservation'> Book A Table</h2>
+
+<div className="reservation__from grid">
+    <div className="form__input-div">
+        <input type="text" placeholder='Your Name' className="form__input" />
     </div>
+
+    <div className="form__input-div">
+        <input type="email" placeholder='Your Email Address' className="form__input" />
+    </div>
+
+    <div className="form__input-div">
+        <input type="text" placeholder='Your Phone' className="form__input" />
+    </div>
+
+    <div className="form__input-div">
+        <input type="text" placeholder='Date Ex: DD/MM/YY' className="form__input" />
+    </div>
+</div>
+    </section>
   )
 }
 
